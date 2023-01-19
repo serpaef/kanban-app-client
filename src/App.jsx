f0908a58ad2028170ecb9e190dde642fc3f3cd7f
Fernando Serpa <serpaef@outlook.com>
2023-01-19 12:20:53 -0300

    importing font

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 function App() {
   return (
     <div>
       What about a Kanban?
     </div>
   );
-}
+};
 
 export default App;
